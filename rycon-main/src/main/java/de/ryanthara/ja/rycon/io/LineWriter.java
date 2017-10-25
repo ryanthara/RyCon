73a3824ea873f102f713330ecb32eaceb331f557
ryanthara <code@ryanthara.de>
2017-10-25 21:02:29 +0200

    Update version number, precise Javadoc comments and implements IllegalArgumentException

--- rycon-main/src/main/java/de/ryanthara/ja/rycon/io/LineWriter.java
+++ rycon-main/src/main/java/de/ryanthara/ja/rycon/io/LineWriter.java
@@ -24,13 +24,13 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 /**
- * Instances of this class provides functions to write an {@code ArrayList<String>} line by line to a file.
+ * Instances of this class provides functions to write an {@code ArrayList<String>} line by line into a file.
  * <p>
  * A couple of things are implemented as additional functionality. At the moment, there is no thread safety
  * implemented or planed.
  *
  * @author sebastian
- * @version 2
+ * @version 3
  * @since 1
  */
 public class LineWriter {
@@ -39,7 +39,7 @@ public class LineWriter {
     private int writtenLines = -1;
 
     /**
-     * Constructs a new instance of this class with the filename as {code String} parameter.
+     * Constructs a new instance of this class with the filename as {@code String} parameter.
      *
      * @param fileName filename as {@code String}
      */
@@ -60,13 +60,19 @@ public class LineWriter {
     /**
      * Writes a given {@code ArrayList<String>} line by line to the file system.
      *
-     * @param lines given list to write to file system
+     * @param lines given list to write into the file
      *
-     * @return success
+     * @return success true if the file is written successfully
+     *
+     * @throws IllegalArgumentException Throws an {@link IllegalArgumentException} when the lines parameter is null
      */
     public boolean writeFile(ArrayList<String> lines) {
         boolean success = false;
         PrintWriter pw = null;
+
+        if (lines == null) {
+            throw new IllegalArgumentException();
+        }
 
         try {
             pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)), true);
